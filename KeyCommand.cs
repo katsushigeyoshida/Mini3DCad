5b018ea232265b2dbc4f5fb39b50b802abb9d9aa
Katsushige Yoshida <k-yosida@g06.itscom.net>
2024-02-10 12:50:35 +0900

    キー入力コマンド追加

---
+++ KeyCommand.cs
@@ -0,0 +1,249 @@
+﻿using CoreLib;
+
+namespace Mini3DCad
+{
+    public class KeyCommand
+    {
+        private List<string> mMainCmd = new List<string>() {
+            "point", "line", "rect", "polyline", "polygon", "arc", "circle", "text",
+            "translate", "rotate", "mirror", "trim", "stretch", "copy", "scaling",
+            "color", "linetype", "thickness", "pointtype", "pointsize", "textsize", "ha", "va"
+        };
+
+        private List<PointD> mPoints = new List<PointD>();
+        private List<int> mPickEnt = new List<int>();
+        private double mRadius = 0;
+        private double mSa = 0;
+        private double mEa = Math.PI * 2;
+        private double mValue = 0;
+        private string mValString = "";
+        private int mCommandNo = -1;
+        private FACE3D mFace = FACE3D.XY;
+
+        public List<string> mKeyCommandList = new();                //  キー入力コマンドの履歴
+        public string mTextString = "";                             //  文字列データ
+        public string mCommandStr = "";
+        public DataManage mDataManage;
+
+        private YCalc ycalc = new YCalc();
+        private YLib ylib = new YLib();
+
+        public KeyCommand(DataManage dataManage)
+        {
+            mDataManage = dataManage;
+        }
+
+        public bool setCommand(string command, FACE3D face)
+        {
+            if (0 < command.Length) {
+                mCommandStr = command;
+                mFace = face;
+            }
+            return false;
+        }
+
+
+        public bool execCommand()
+        {
+            getCommandParameter(mCommandStr);
+            switch (mMainCmd[mCommandNo]) {
+                case "line":
+                    if (1 < mPoints.Count)
+                        mDataManage.addLine(mPoints[0], mPoints[1]);
+                    break;
+                case "rect":
+                    if (1 < mPoints.Count)
+                        mDataManage.addRect(mPoints[0], mPoints[1]);
+                    break;
+                case "circle":
+                    if (1 < mPoints.Count)
+                        mDataManage.addCircle(mPoints[0], mPoints[1]);
+                    break;
+                case "arc":
+                    if (2 < mPoints.Count)
+                        mDataManage.addArc(mPoints[0], mPoints[1], mPoints[2]);
+                    break;
+                case "plyline":
+                    if (1 < mPoints.Count)
+                        mDataManage.addPolyline(mPoints);
+                    break;
+                case "polygon":
+                    if (2 < mPoints.Count)
+                        mDataManage.addPolygon(mPoints);
+                    break;
+                default:
+                    return false;
+            }
+            mDataManage.updateData();
+            return true;
+        }
+
+        /// <summary>
+        /// コマンド文字列からコマンドやパラメータ値を求める
+        /// </summary>
+        /// <param name="command"></param>
+        private void getCommandParameter(string command)
+        {
+            mCommandNo = -1;
+            mRadius = 0;
+            mSa = 0;
+            mEa = 0;
+            mValue = 0;
+            mValString = "";
+            mPickEnt.Clear();
+            mPoints.Clear();
+            List<string> cmd = commandSplit(command);
+            for (int i = 0; i < cmd.Count; i++) {
+                if (0 > cmd[i].IndexOf("\""))
+                    cmd[i] = cmd[i].ToLower();
+                if (mCommandNo < 0) {
+                    mCommandNo = mMainCmd.FindIndex(p => 0 <= p.IndexOf(cmd[i]));
+                    continue;
+                }
+                if (0 == cmd[i].IndexOf("x") || 0 == cmd[i].IndexOf("y") ||
+                    0 == cmd[i].IndexOf("dx") || 0 == cmd[i].IndexOf("dy")) {
+                    //  座標/相対座標
+                    PointD dp = getPoint(cmd[i],
+                        mPoints.Count < 1 ? new PointD(0, 0) : mPoints[mPoints.Count - 1]);
+                    if (!dp.isNaN())
+                        mPoints.Add(dp);
+                } else if (0 == cmd[i].IndexOf("p")) {
+                    //  要素番号
+                    mPickEnt.Add(getIntPara(cmd[i], "p"));
+                } else if (0 == cmd[i].IndexOf("r")) {
+                    //  半径
+                    mRadius = getPara(cmd[i], "r");
+                } else if (0 == cmd[i].IndexOf("sa")) {
+                    //  始角
+                    mSa = getPara(cmd[i], "sa");
+                } else if (0 == cmd[i].IndexOf("ea")) {
+                    //  終角
+                    mEa = getPara(cmd[i], "ea");
+                } else if (char.IsDigit(cmd[i][0]) || cmd[i][0] == '-') {
+                    //  数値
+                    mValue = ylib.string2double(cmd[i]);
+                } else if (cmd[i][0] == '"') {
+                    //  文字列
+                    mTextString = cmd[i].Trim('"');
+                } else {
+                    //  その他の文字列
+                    mValString = cmd[i];
+                }
+            }
+        }
+
+        /// <summary>
+        /// パラメータ文字列からPointDの値に変換(計算式可)
+        /// </summary>
+        /// <param name="xy">パラメータ文字列</param>
+        /// <param name="a">パラメータ名</param>
+        /// <param name="b">パラメータ名</param>
+        /// <returns>パラメータ値</returns>
+        private PointD getPoint(string xy, string a = "x", string b = "y")
+        {
+            int xn = xy.IndexOf(a);
+            int yn = xy.IndexOf(b);
+            double x = ycalc.expression(xy.Substring(xn + a.Length, yn - a.Length));
+            double y = ycalc.expression(xy.Substring(yn + b.Length));
+            return new PointD(x, y);
+        }
+
+        /// <summary>
+        /// パラメータ文字列(座標/相対座標)からPointDの値に変換(計算式可)
+        /// </summary>
+        /// <param name="xy">パラメータ文字列</param>
+        /// <param name="prev">前座標</param>
+        /// <returns>座標</returns>
+        private PointD getPoint(string xy, PointD prev)
+        {
+            PointD p = new PointD();
+            string[] sep = { "x", "y", "dx", "dy", ",", " " };
+            List<string> list = ylib.splitString(xy, sep);
+            for (int i = 0; i < list.Count; i++) {
+                if (list[i] == "x" && i + 1 < list.Count) {
+                    p.x = ycalc.expression(list[++i]);
+                } else if (list[i] == "y" && i + 1 < list.Count) {
+                    p.y = ycalc.expression(list[++i]);
+                } else if (list[i] == "dx" && i + 1 < list.Count) {
+                    p.x = ycalc.expression(list[++i]) + prev.x;
+                } else if (list[i] == "dy" && i + 1 < list.Count) {
+                    p.y = ycalc.expression(list[++i]) + prev.y;
+                }
+            }
+
+            return p;
+        }
+
+        /// <summary>
+        /// パラメータ文字列から値に変換(計算式可)
+        /// </summary>
+        /// <param name="paraStr">パラメータ文字列</param>
+        /// <param name="para">パラメータの種類</param>
+        /// <returns>パラメータ値</returns>
+        private double getPara(string paraStr, string para)
+        {
+            int rn = paraStr.IndexOf(para);
+            double r = ycalc.expression(paraStr.Substring(rn + 1));
+            return r;
+        }
+
+        /// <summary>
+        /// パラメータ文字列から整数値に変換
+        /// </summary>
+        /// <param name="paraStr">パラメータ文字列</param>
+        /// <param name="para">パラメータの種類</param>
+        /// <returns>パラメータ値</returns>
+        private int getIntPara(string paraStr, string para)
+        {
+            int pn = paraStr.IndexOf(para);
+            int n = (int)ycalc.expression(paraStr.Substring(pn + 1));
+            return n;
+        }
+
+        /// <summary>
+        /// コマンド文字列をコマンドやパラメータなどに分解
+        /// </summary>
+        /// <param name="command">コマンド文字列</param>
+        /// <returns>パラメータリスト</returns>
+        private List<string> commandSplit(string command)
+        {
+            List<string> cmd = new List<string>();
+            string buf = "";
+            for (int i = 0; i < command.Length; i++) {
+                if (command[i] == ' ' || command[i] == ',') {
+                    if (0 < buf.Length) {
+                        cmd.Add(buf);
+                        buf = "";
+                    }
+                } else if (command[i] == '"') {
+                    if (0 < buf.Length) {
+                        cmd.Add(buf);
+                        buf = "";
+                    }
+                    buf += command[i++];
+                    do {
+                        buf += command[i];
+                    } while (i < command.Length - 1 && command[i++] != '"');
+                    cmd.Add(buf);
+                    buf = "";
+                } else {
+                    buf += command[i];
+                }
+            }
+            if (0 < buf.Length) {
+                cmd.Add(buf);
+                buf = "";
+            }
+            return cmd;
+        }
+
+        public List<string> keyCommandList(string command)
+        {
+            int n = mKeyCommandList.IndexOf(command);
+            if (0 <= n)
+                mKeyCommandList.RemoveAt(n);
+            mKeyCommandList.Insert(0, command);
+            return mKeyCommandList;
+        }
+    }
+}
